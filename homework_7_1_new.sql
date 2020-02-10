05fd9fe85aaf14ff66c96a67e9be4936c2819958
Natalia Ramazanova <eva0511@rambler.ru>
2020-02-10 22:21:29 +0300

    Исправила недочеты Домашнего задания к уроку 7

---
+++ homework_7_1_new.sql
@@ -0,0 +1,43 @@
+-- Составьте список пользователей users, которые осуществили хотя бы один заказ orders в интернет магазине.
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Имя покупателя',
+  birthday_at DATE COMMENT 'Дата рождения',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Покупатели';
+
+INSERT INTO users (name, birthday_at) VALUES
+  ('Геннадий', '1990-10-05'),
+  ('Наталья', '1984-11-12'),
+  ('Александр', '1985-05-20'),
+  ('Сергей', '1988-02-14'),
+  ('Иван', '1998-01-12'),
+  ('Мария', '1992-08-29');
+  
+ DROP TABLE IF EXISTS orders;
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY,
+  user_id INT UNSIGNED,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY index_of_user_id(user_id)
+) COMMENT = 'Заказы';
+INSERT INTO orders (user_id)
+VALUES (1),
+       (1),
+       (2),
+       (3),
+       (3),
+       (4),
+       (4),
+       (5),
+       (5),
+       (5),
+       (4);
+       
+SELECT DISTINCT name
+FROM users u
+JOIN orders o on o.user_id = u.id
+ORDER by name; 
